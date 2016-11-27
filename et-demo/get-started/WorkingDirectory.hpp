2bdd12b99eb0a5bff4bb05aa434a82a3ffeba61e
Sergey Reznik <rezniksa@gmail.com>
2016-11-27 23:51:08 +0300

    Initial commit: demos moved from et repo, vs2015 project/solutuion setup

---
+++ et-demo/get-started/WorkingDirectory.hpp
@@ -0,0 +1,3 @@
+#pragma once 
+/* auto-generated file */ 
+const char* workingDirectory = R"(Q:\Projects\et-demo\et-demo\get-started\)"; 
---
+++ et-demo/get-started/source/main.cpp
@@ -0,0 +1,6 @@
+#include "MainController.h"
+
+int main(int argc, char * argv[])
+{
+	return et::application().run(argc, argv);
+}
